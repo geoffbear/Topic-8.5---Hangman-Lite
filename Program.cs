a57f957eb2b4d3177c4b45a41e29226487b08a94
339470494 <339470494@2260I-L29-006>
2024-05-17 14:37:43 -0400

    done

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Topic_8._5___Hangman_Lite
             bool stopPlaying = false;
             int guesses = 0;
             List<string> wordDisplay = new List<string>();
+            List<string> lettersGuessed = new List<string>();
             wordDisplay.Add("_ ");
             wordDisplay.Add("_ ");
             wordDisplay.Add("_ ");
@@ -30,52 +31,86 @@ namespace Topic_8._5___Hangman_Lite
             {
                 while (!stopPlaying && guesses != 7)
                 {
+
                     Console.Clear();
                     Console.WriteLine("Guess the secert word, letter by letter:");
                     Console.WriteLine();
                     Console.WriteLine();
-
+                    HangmanGuesses(guesses);
                     Console.WriteLine();
                     for (int i = 0; i < wordDisplay.Count; i++)
                         Console.Write(wordDisplay[i]);
 
                     Console.WriteLine();
                     Console.WriteLine();
+                    Console.WriteLine("Letters guessed: ");
+                    for (int i = 0; i < lettersGuessed.Count; i++)
+                        Console.Write(lettersGuessed[i] + ", ");
+
+                    Console.WriteLine();
+                    Console.WriteLine("Guesses used: " + guesses + "/7");
                     Console.Write("Guess here: ");
                     guessletter = Console.ReadLine().ToUpper().Trim();
+                    Console.WriteLine();
 
 
+                    if (guessletter.ToUpper() == "C" )
+                    {
+                        wordDisplay[0] = "C ";
+                        wordDisplay[3] = "C ";
                     }
 
                     else if (guessletter.ToUpper() == "A")
                     {
-
+                        wordDisplay[1] = "A ";
                     }
 
                     else if (guessletter.ToUpper() == "L")
                     {
-
+                        wordDisplay[2] = "L ";
+                        wordDisplay[5] = "L ";
                     }
 
                     else if (guessletter.ToUpper() == "U")
                     {
-
+                        wordDisplay[4] = "U ";
+                        wordDisplay[6] = "U ";
                     }
 
                     else if (guessletter.ToUpper() == "S")
                     {
-
+                        wordDisplay[7] = "S";
                     }
 
                     else if (guessletter.Length > 1 || !Regex.IsMatch(guessletter, @"^[a-zA-Z]+$"))
                     {
                         Console.WriteLine("Invalid character, please guess again.");
                     }
+
                     else
                     {
                         guesses++;
+                        lettersGuessed.Add(guessletter);
                     }
+
+                    if (wordDisplay[0].Contains("C") && wordDisplay[1].Contains("A") && wordDisplay[2].Contains("L") && wordDisplay[4].Contains("U") && wordDisplay[7].Contains("S"))
+                    {   
+                        wordDisplay[0] = "C ";
+                        wordDisplay[1] = "A ";
+                        wordDisplay[2] = "L ";
+                        wordDisplay[3] = "C ";
+                        wordDisplay[4] = "U ";
+                        wordDisplay[5] = "L ";
+                        wordDisplay[6] = "U ";
+                        wordDisplay[7] = "S";
+                        Console.WriteLine("You have won!!! Congrats!!! (Press Enter to continue)");
+                        Console.ReadLine();
+
+                        stopPlaying = true;
+                    }
+
                 }
+                Console.WriteLine("The word was " + word + ". Thanks for playing.");
             }
 
             else
@@ -85,45 +120,41 @@ namespace Topic_8._5___Hangman_Lite
             }
         }
 
-
+       
         public static void HangmanGuesses(int guesses)
         {
             switch (guesses)
             {
                     case 0: 
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n      |\r\n      |\r\n      |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 1: 
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n      |\r\n      |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 2: 
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n  |   |\r\n      |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 3: 
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|   |\r\n      |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 4: 
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n      |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 5:
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n /    |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 6:
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n  |   |\r\n  O   |\r\n /|\\  |\r\n / \\  |\r\n      |\r\n=========\r\n");
                     break;
 
                     case 7:
-                    Console.WriteLine("");
-                    break;
-
-                    case 8:
-                    Console.WriteLine("");
+                    Console.WriteLine("  +---+\r\n      |\r\n      | \r\n \\O/  |\r\n  |   |\r\n / \\  |\r\n=========");
                     break;
             }
         }
